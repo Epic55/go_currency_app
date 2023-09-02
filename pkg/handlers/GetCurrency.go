408ee58cf25a18927b67debaae66ebb12fd83480
Alibek Ismailov <alibek_ismailov@epam.com>
2023-09-02 09:55:42 +0600

    Sat 09/02/2023 -  9:55:41.66

--- pkg/handlers/GetCurrency.go
+++ pkg/handlers/GetCurrency.go
@@ -9,24 +9,7 @@ import (
 
 	"github.com/Epic55/go_project_task/pkg/models"
 	"github.com/gorilla/mux"
-	//"os"
 )
-
-type Rates struct {
-	XMLName xml.Name `xml:"rates"`
-	Date    string   `xml:"date"`
-	Rates   []s1     `xml:"item"`
-}
-
-type s1 struct {
-	XMLName     xml.Name `xml:"item"`
-	Fullname    string   `xml:"fullname"`
-	Title       string   `xml:"title"`
-	Description string   `xml:"description"`
-	Quant       int      `xml:"quant"`
-	Index       int      `xml:"index"`
-	Change      float64  `xml:"change"`
-}
 
 func (h handler) GetCurrency(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
@@ -43,20 +26,21 @@ func (h handler) GetCurrency(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
-	var var1 Rates
+	var var1 models.Rates
 	xml.Unmarshal(responseData, &var1)
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	//SAVE TO DB
 	var var2 models.R_CURRENCY
 	xml.Unmarshal(responseData, &var2)
 
 	if result := h.DB.Create(&var2); result.Error != nil {
 		fmt.Println(result.Error)
 	}
-
+	fmt.Println(responseData)
 	w.Header().Add("Content-Type", "application/xml")
 	w.WriteHeader(http.StatusOK)
 	xml.NewEncoder(w).Encode(var1)
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,5 +1,7 @@
 package models
 
+import "encoding/xml"
+
 type Book struct {
 	Id     int    `json:"id" gorm:"primaryKey"`
 	Title  string `json:"title"`
@@ -7,13 +9,19 @@ type Book struct {
 	Desc   string `json:"desc"`
 }
 
+type Rates struct {
+	XMLName xml.Name     `xml:"rates"`
+	A_date  string       `xml:"date"`
+	Rates   []R_CURRENCY `xml:"item"`
+}
+
 type R_CURRENCY struct {
-	Id          int     `xml:"id" gorm:"primaryKey"`
-	Fullname    string  `xml:"fullname"`
-	Title       string  `xml:"title"`
-	Description string  `xml:"description"`
-	Quant       int     `xml:"quant"`
-	Index       int     `xml:"index"`
-	Change      float64 `xml:"change"`
-	A_date      string  `xml:"date"`
+	Id          int    `xml:"id" gorm:"primaryKey"`
+	Fullname    string `xml:"fullname"`
+	Title       string `xml:"title"`
+	Description string `xml:"description"`
+	Quant       int    `xml:"quant"`
+	Index       int    `xml:"index"`
+	Change      string `xml:"change"`
+	A_date      string `xml:"date"`
 }
